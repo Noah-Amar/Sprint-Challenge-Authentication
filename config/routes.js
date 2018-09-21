a7a2762995402dc344347f82055ddc9e1b8f68bf
Noah-Amar <ne.amar@outlook.com>
2018-09-21 12:46:41 -0500

    mvp + working on finishing stretch

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,8 @@
 const axios = require('axios');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const db = require("../database/dbConfig.js");
+const jwtKey = require("../_secrets/keys").jwtKey;
 
 const { authenticate } = require('./middlewares');
 
@@ -8,12 +12,61 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
+function generateToken(user) {
+  const payload = {
+    username: user.username
+  };
+  const options = {
+    expiresIn: "4h",
+    jwtid: "33333",
+    subject: `${user.id}`
+  };
+  return jwt.sign(payload, jwtKey, options);
+}
+
 function register(req, res) {
   // implement user registration
+  const credentials = req.body;
+  const hash = bcrypt.hashSync(credentials.password, 14);
+  credentials.password = hash;
+  db("users")
+    .insert(credentials)
+    .then(ids => {
+      const id = ids[0];
+
+      db("users")
+        .where({ id })
+        .first()
+        .then(user => {
+          const token = generateToken(user);
+          res.status(201).json({ id: user.id, token });
+        })
+        .catch(err => {
+          res.status(500).json(err);
+        });
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 }
 
 function login(req, res) {
   // implement user login
+  const credentials = req.body;
+  db("users")
+    .where({ username: credentials.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(credentials.password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ token });
+      } else {
+        res.status(401).json({ message: "Credentials denied!" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 }
 
 function getJokes(req, res) {
